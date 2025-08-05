274b57a57dc6ad42b698ef827fbefc47592f134e
Micks-Tech <mickstech060924@gmail.com>
2025-08-05 22:55:25 +0800

    login web

---
+++ Js/login.js
@@ -0,0 +1,17 @@
+
+function login() {
+    var user = document.getElementById("user").value;
+    var pass = document.getElementById("pass").value;
+
+    if (user === '' || pass === '') {
+        alert('Please fill ot this fields');
+        return;
+    } 
+    if (user === 'Admin' && pass === '12345') {
+        alert('Successfully to logged in');
+        window.location.href = 'button.html';
+    } else {
+        alert('Wrong Cerdentials!');
+        window.location.href = 'login.html';
+    }
+}
