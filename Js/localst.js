0e09991aead93e49f234f2472e42cc3bb9e79c3f
Micks-Tech <mickstech060924@gmail.com>
2025-08-14 15:55:26 +0800

    localStorage

---
+++ Js/localst.js
@@ -0,0 +1,5 @@
+//localStorage.setItme('lastname', 'smith');
+//document.getElementById('demo').innerHTML = localStorage.getItem('lastname')
+localStorage.setItem('lastname', 'smith');
+
+document.getElementById("demo").innerHTML = localStorage.getItem('lastname');
